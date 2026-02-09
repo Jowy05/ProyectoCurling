3a508bae1d3c784c1058689ee675aeab55b23341
JOEL\jowyd <jowydelolmo@gmail.com>
2026-02-09 18:57:41 +0100

    commit 1

---
+++ src/main/java/com/urijoel/curling/CurlingApplication.java
@@ -0,0 +1,13 @@
+package com.urijoel.curling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urijoel/curling/CurlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urijoel.curling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
