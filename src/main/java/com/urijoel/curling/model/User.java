d36deebd29c1f60cdd1034619aa6ac239266316a
DESKTOP-ECAODA6\Uri <oriolcv2003@gmail.com>
2026-02-18 18:44:26 +0100

    dto

--- src/main/java/com/urijoel/curling/model/User.java
+++ src/main/java/com/urijoel/curling/model/User.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+//
 @Data
 @Builder
 @NoArgsConstructor
