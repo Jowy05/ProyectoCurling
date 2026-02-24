ca954206cabb51a6d606df8e403b829777b37b48
DESKTOP-ECAODA6\Uri <oriolcv2003@gmail.com>
2026-02-24 16:29:49 +0100

    Comentarios y ajustes

--- src/main/java/com/urijoel/curling/dto/LoginRequestDTO.java
+++ src/main/java/com/urijoel/curling/dto/LoginRequestDTO.java
@@ -10,9 +10,21 @@ public class LoginRequestDTO {
 
     public LoginRequestDTO() {}
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
 }
